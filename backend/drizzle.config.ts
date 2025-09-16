1a1ddbc99acc9fc8f04c7659354aa669becb655d
Asam <abbasaliaboubakar@gmail.com>
2025-09-16 13:14:32 +0100

    fix docker

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -10,7 +10,7 @@ import {
 } from "./src/startup/config";
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/schema",
+  schema: "./backend/src/schema",
   dialect: "postgresql",
   dbCredentials: {
     url: `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${HOST}:${POSTGRES_PORT}/${DATABASE}`,
